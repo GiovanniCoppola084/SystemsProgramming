b8765678c3233a3e4e1944b16660ad95d1135221
GiovanniCoppola084 <gac6151@rit.edu>
2023-03-23 15:44:20 -0400

    File structure files added.

---
+++ FileStructure/file_template.h
@@ -0,0 +1,21 @@
+/*
+** SCCS ID: @(#)file_template.h	3/23/2023
+**
+** File:    file_template.h
+**
+** Authors: Gino
+**
+** Description: The prototypes for functions to set up a basic file.
+** Base of Unix V7 or Microsoft FAT
+** https://www.cs.rit.edu/~wrc/courses/common/notes/csci452/
+** https://learn.microsoft.com/en-us/troubleshoot/windows-client/backup-and-storage/fat-hpfs-and-ntfs-file-systems
+** https://github.com/v7unix/v7unix
+** https://www.tuhs.org/cgi-bin/utree.pl?file=V7
+*/
+
+#ifndef _FILE_TEMPLATE_H_
+#define _FILE_TEMPLATE_H_
+
+// Start of the prototypes
+
+#endif
