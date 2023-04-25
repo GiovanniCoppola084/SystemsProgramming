208d9537a647d612871ea31de3e94823c8b3f067
GiovanniCoppola084 <gac6151@rit.edu>
2023-04-25 14:26:05 -0400

    Some error checking done.

--- FileStructure/file_template.c
+++ FileStructure/file_template.c
@@ -1,31 +1,26 @@
 #include "file_template.h"
-#include "kmem.h"
 
-/**
-* file_system_init - this function will initialize the file system by providing the free blocks
-*                    that will be used for the inodes and the data blocks. The used blocks linked
-*                    list will be null since we won't have any in use at init.
-* 
-* @return the file system will the initialized linked lists for memory
-*/
-Filesystem_s* file_system_init (void) {
-    Filesystem_s *fs = (Filesystem_s *) (USERLAND_FILE_ADDRESS_START + 1);
+FileSystem_s* file_system_init (void) {
+    FileSystem_s *fs = (FileSystem_s *) (USERLAND_FILE_ADDRESS_START + 0x1);
 
     // Set the initial number of blocks
+    // Implicit coversion from 'int' to 'uint8_t' (aka 'unsigned char') changes value from 960 to 192
     fs->num_free_blocks = MAX_NUM_OF_DATA_BLOCKS;
     // Set the initial number of free nodes
     fs->num_free_nodes = TOTAL_NUM_OF_INODES;
 
     // Add inode sector given the size of the inodes
     // This is the start plus the 16 bits for the file systems struct
-    uint32_t current_address = USERLAND_FILE_ADDRESS_START +  SIZE_OF_FILE_SYSTEM + 1;
+    uint32_t current_address = USERLAND_FILE_ADDRESS_START +  SIZE_OF_FILE_SYSTEM_HEX + 0x1;
 
     // Set the free nodes to be the head of the list now
     fs->free_nodes = NULL;
 
     for (int i = 0; i < TOTAL_NUM_OF_INODES; i++) {
+        // Can I assign the pointer like this?
+        // This will only cast the pointer in that address. This might need to be fixed
+        // It should operate like malloc
         list_s *new_node = (list_s *) current_address;
-        current_address = current_address + SIZE_OF_INODES + 1;
         assert(new_node != NULL);
 
         new_node->data = NULL;
@@ -40,19 +35,21 @@ Filesystem_s* file_system_init (void) {
             }
             current->next = new_node;
         }
+
+        // Move to the next inode space
+        current_address = current_address + SIZE_OF_INODES_HEX + 0x1;
     }
 
     // Set the used nodes to null since we wont be using any at init
     fs->used_nodes = NULL;
 
     // Move it up one byte plus the size of an inode to get into the block space
-    current_address = current_address + SIZE_OF_INODE + 1;
+    // current_address = current_address + SIZE_OF_INODES_HEX + 0x1;
 
     fs->free_blocks = NULL;
 
-    for (int i = 0; i < TOTAL_NUM_OF_DATA_BLOCKS; i++) {
+    for (int i = 0; i < MAX_NUM_OF_DATA_BLOCKS; i++) {
         list_s *new_node = (list_s *) current_address;
-        current_address = current_address + SIZE_OF_DATA_BLOCK + 1;
         assert(new_node != NULL);
 
         new_node->data = NULL;
@@ -67,97 +64,227 @@ Filesystem_s* file_system_init (void) {
             }
             current->next = new_node;
         }
+    
+        // Move to the next data block space
+        current_address = current_address + SIZE_OF_DATA_BLOCK_HEX + 0x1;
     }
 
     fs->used_blocks = NULL;
 
+    fs->current_inode = NULL;
+    fs->previous_inode = NULL;
+
     return fs;
 }
 
-void create_inode(FileSystem_s *fs, uint8_t num_pointers, bool_t is_direct) {
-    assert(fs->free_nodes == NULL); // Assert that we still have free nodes. If we don't, then we panic
-    
-    list_s *new_node = fs->free_nodes;
+Inode_s *create_inode(FileSystem_s *fs, unsigned char *name, unsigned char *block, uint8_t index, bool_t is_direct, Inode_s *new_inode) {
+    assert(fs->free_nodes != NULL); // Assert that we still have free nodes. If we don't, then we panic
 
-    if (fs->used_nodes != NULL) {
+    list_s *new_node = fs->free_nodes;
+    list_s *current;
+
+    if (fs->used_nodes == NULL) {
         fs->used_nodes = new_node;
         fs->free_nodes = new_node->next;
+
+        current = fs->used_nodes;
     } else {
-        list_s *current = fs->used_nodes;
+        current = fs->used_nodes;
         while (current->next) {
             current = current->next;
         }
         current->next = new_node;
-
-        assert(fs->used_nodes != NULL);
-
-        current = current->next;
-        current->data = (Inode_s *) current->data;
-        current->data->size = 0;
-        current->data->num_of_pointers = 0;
-        current->data->is_direct = false;
-        for (int i = 0; i < 16; i++) {
-            current->data->direct[i] = NULL;
-        }
-
-        fs->free_nodes = new_node->next;
     }
+
+    assert(current->next != NULL);
+
+    current = current->next;
+    Inode_s *accessed_node = (Inode_s *)(current->data);
+    // Member reference base type 'void' is not a structure or a union
+    accessed_node->size = 0;
+    accessed_node->num_of_pointers = 0;
+
+    for (int i = 0; i < POINTERS_PER_INODE; i++) {
+        accessed_node->direct[i] = NULL;
+    }
+
+    fs->free_nodes = new_node->next;
 
     fs->num_free_nodes--;
-    fs->num_used_nodes++;
+
+    return (Inode_s *)current->data;
 }
 
-void create_data_block(FileSystem_s *fs, unsigned char *name, unsigned char *block, uint8_t index) {
+void delete_pointer_in_inode(FileSystem_s *fs, Inode_s *inode, uint8_t index, bool_t is_direct) {
     assert(fs->free_blocks != NULL);
+    assert(inode != NULL);
+    
+    if (is_direct) {
+        inode->direct[index] = NULL;
+        
+        list_s *free_node = fs->free_nodes;
+        while (free_node->next) {
+            free_node = free_node->next;
+        }
+
+        list_s *used_node = fs->used_blocks;
+        bool_t found = false;
+        while (used_node->next) {
+            // Is this the right way to type cast in this case?
+            if (((Inode_s *)(used_node->data)) == inode) {
+                free_node->next = used_node;
+                found = true;
+                break;
+            }
+        }
+
+        assert(found == true); 
+        assert(free_node->next != NULL);
+
+        free_node = free_node->next;
+        free_node->data = NULL;
+        free_node->next = NULL;
+        fs->num_free_nodes++;
+    } else {
+        inode->direct[index] = NULL;
+
+        list_s *free_block = fs->free_blocks;
+        while (free_block->next) {
+            free_block = free_block->next;
+        }
+
+        list_s *used_block = fs->used_blocks;
+        bool_t found = false;
+        while (used_block->next) {
+            if (((File_s *)(used_block->data)) == inode) {
+                free_block->next = used_block;
+                found = true;
+                break;
+            }
+        }
+
+        assert(found == true);
+        assert(free_block->next != NULL);
+
+        free_block = free_block->next;
+        free_block->data = NULL;
+        free_block->next = NULL;
+        fs->num_free_blocks++;
+    }
+}
+
+list_s *create_data_block(FileSystem_s *fs, Inode_s *inode, bool_t is_direct, unsigned char *name, unsigned char *block, uint8_t index) {
+    assert(fs->free_blocks != NULL);
+    if (index == 0) {
+        assert(is_direct);
+    }
 
     list_s *new_node = fs->free_blocks;
+    list_s *current;
 
-    if (fs->used_blocks != NULL) {
+    if (fs->used_blocks == NULL) {
         fs->used_blocks = new_node;
         fs->free_blocks = new_node->next;
+
+        current = fs->used_blocks;
     } else {
-        list_s *current = fs->used_blocks;
+        current = fs->used_blocks;
         while (current->next) {
             current = current->next;
         }
         current->next = new_node;
-
-        assert(fs->used_nodes != NULL);
-
-        current = current->next;
-        current->data = (File_s *) current->data;
-        current->data->name = name;
-        current->data->file_index = index;
-        current->data->data_block = block;
-
-        fs->free_blocks = new_node->next;
     }
 
+    assert(current->next != NULL);
+
+    current = current->next;
+    Inode_s *accessed_node = (File_s *)(current->data);
+    accessed_node->name = name;
+    accessed_node->file_index = index;
+    accessed_node->data_block = block;
+
+    fs->free_blocks = new_node->next;
+
     fs->num_free_blocks--;
-    fs->num_used_blocks++;
+
+    inode->direct[index] = current;
+
+    return current;
 }
 
-/**
-* Output what is in the file system to a data block
-*/
-void inode_read (FileSystem_s *fs, uint32_t inode_number, char *data_block, uint32_t length) {
+void inode_read (FileSystem_s *fs, Inode_s *inode, uint32_t inode_number) {
     // This will be accessing the data block in a file, rather than accessing another inode
     // So if the mode bit tells you that it is a direct pointer, get the data block
     // If it's not, then we panic and abort
+    if (inode_number == 0) {
+        assert(inode->is_direct);
+    }
+
+    File_s *file = (File_s *) inode->direct[inode_number];
+
+    printf("Data in the block: %s", file->data_block);
+}
+
+void inode_write (FileSystem_s *fs, Inode_s *inode, uint32_t inode_number, unsigned char *block) {
+    // This will work similar to read. If the inode is not a direct pointer, then we panic
+    // If it is direct, then we simply set the data block
+    if (inode_number == 0) {
+        assert(inode->is_direct);
+    }
+
+    File_s *file = (File_s *) inode->direct[inode_number];
+
+    file->data_block = block;
+    printf("Data now in block: %s", file->data_block);
+}
+
+void inode_delete_data (FileSystem_s *fs, Inode_s *inode, uint32_t inode_number) {
+    if (inode_number == 0) {
+        assert(inode->is_direct);
+    }
+
+    File_s *file = (File_s *) inode->direct[inode_number];
+
+    unsigned char empty_block[SIZE_OF_DATA_BLOCK];
+    for (int i = 0; i < SIZE_OF_DATA_BLOCK; i++) {
+        empty_block[i] = ' ';
+    }
     
+    file->data_block = &empty_block[0];
 }
 
-/**
-* Read what is in the data block to the file
-*/
-void inode_write (FileSystem_s *fs, uint32_t inode_number, char *data_block, uint32_t length) {
+Inode* move_in_directory (FileSystem_s *fs, Inode_s *inode) {
+    assert(!inode->is_direct);
 
+    fs->previous_inode = inode;
+    fs->current_inode = (Inode_s *) inode->direct[0];
+
+    assert(fs->current_index != NULL);
+
+    // Return the first pointer if it's not direct, since that will move us to a new directory
+    return fs->current_inode;
 }
 
-Inode* move_in_directory (Filesystem_s *fs, uint32_t inode_number) {
-    
+void print_directory (FileSystem_s *fs, Inode_s *inode) {
+    printf("Current working directory: \n");
+    for (int i = 0, i < POINTERS_PER_INODE, i++) {
+        if (i == 0 && !inode->is_direct && inode->direct[0] != NULL) {
+            printf("1. Directory: %s", inode->direct[0]->name);
+        } else if (i == 0 && inode->is_direct && inode->direct[0] != NULL) {
+            printf("1. File: %s", inode->direct[0]->name);
+        }
+
+        if (i > 0 && inode->direct[i] != NULL) {
+            printf("%d. File: %s", (i + 1), inode->direct[i]->name);
+        }
+    }
+
+    printf("Total inodes in use: %d\n", (TOTAL_NUM_OF_INODES - fs->num_free_nodes));
+    printf("Total free inodes: %d\n", fs->num_free_nodes);
+    printf("Total blocks in use: %d\n", (MAX_NUM_OF_DATA_BLOCKS - fs->num_free_blocks));
+    printf("Total free blocks: %d\n", fs->num_free_blocks);
 }
 
-Inode* mode_out_directory (FileSystem_s *fs, uint32_t inode_number) {
-
-}
+// Might not be able to implement this unless I do a doubly linked list, or a if I store the previous in the file 
+// system structure
+Inode* mode_out_directory (FileSystem_s *fs, Inode_s *inode);
--- FileStructure/file_template.h
+++ FileStructure/file_template.h
@@ -5,8 +5,8 @@
 **
 ** Authors: Gino Coppola
 **
-** Description: The prototypes for functions to set up a basic file.
-** Base of Unix V7 or Microsoft FAT
+** Description: My take on a Unix V7 file structure using ramdisk in our operating system
+** 
 ** https://www.cs.rit.edu/~wrc/courses/common/notes/csci452/
 ** https://learn.microsoft.com/en-us/troubleshoot/windows-client/backup-and-storage/fat-hpfs-and-ntfs-file-systems
 ** https://github.com/v7unix/v7unix
@@ -19,34 +19,49 @@
 #ifndef _FILE_TEMPLATE_H_
 #define _FILE_TEMPLATE_H_
 
-#include "cio.h"
-#include "users.h"
-#include "ulib.h"
-#include "vector.h"
+#include "../cio.h"
+#include "../users.h"
+#include "../ulib.h"
+#include "../vector.h"
 
+/*File address space start and end*/
 #define USERLAND_FILE_ADDRESS_START 0xC0000000
 #define USERLAND_FILE_ADDRESS_END 0xffffffff
-#define MAX_NUM_OF_DATA_BLOCKS 960
-#define SIZE_OF_DATA_BLOCK 512
-#define TOTAL_SIZE_OF_ALL_DATA_BLOCKS (MAX_NUM_OF_DATA_BLOCKS * TOTAL_SIZE_DATA_BLOCK)
+
+/*Sizes for data blocks*/
+#define MAX_NUM_OF_DATA_BLOCKS 960 // 15 pointers per inode and 64 inodes total
+#define SIZE_OF_DATA_BLOCK_DEC 528
+#define SIZE_OF_DATA_BLOCK_HEX 0x210
+
+/*Sizes for inodes*/
 #define TOTAL_NUM_OF_INODES 64
-#define SIZE_OF_INODES 64
-#define TOTAL_SIZE_FILE_SYS_STRUCT (1 + 1 + (4 * MAX_NUM_OF_DATA_BLOCKS) + (4 * TOTAL_NUM_OF_INODES))
+#define SIZE_OF_INODES_DEC 64
+#define SIZE_OF_INODES_HEX 0x40
+#define POINTERS_PER_INODE 15
+
+/*Sizes for file system structure*/
+#define SIZE_OF_FILE_SYSTEM_DEC 28 // In bytes
+#define SIZE_OF_FILE_SYSTEM_HEX 0x1C
 
 typedef struct Inode_s {
     uint8_t size;
     uint8_t num_of_pointers;
-    uint8_t is_direct; // True for direct, false for indirect. This is only for the first pointer
-    File_s *direct[15]; // 15 pointers to other inodes per inode (MAX)
+    uint8_t is_direct; // Only the first block can be an indirect pointer, this will be true if it's direct, false if indirect
+    void *direct[15]; // 15 pointers to other inodes per inode (MAX)
 } Inode_s; // Exactly 64 bytes
 
 typedef struct File_s {
     unsigned char name[14];
     uint8_t file_index; // 2 bytes for the file index
-    // unsigned char data_block[DATA_BLOCK_SIZE]; // 512 bytes per data block
-    // pointer to data block
-    uint32_t *data_block; // pointer to 512 byte data block
-} File_s;
+    unsigned char data_block[512]; // pointer to 512 byte data block
+} File_s; // 528 bytes
+
+typedef struct list_s {
+    // This will point to either a data section or another node
+    // This will depend on what the mode byte is
+    void *data;
+    struct list_s *next;
+} list_s; 
 
 // Don't want file system as struct
 // Data block is linked list that points to next
@@ -54,65 +69,105 @@ typedef struct File_s {
 // Take the first one off the free list when I need it
 // Add the new one to the top of the free list when I am done with it
 typedef struct FileSystem_s {
-    uint8_t num_free_blocks;
-    uint8_t num_free_nodes;
+    uint32_t num_free_blocks;
+    uint32_t num_free_nodes;
     list_s *free_nodes; // This will contain all of the free nodes
     list_s *used_nodes; // This will be empty at the initial process
     list_s *free_blocks; // This will contain all of the free blocks
     list_s *used_blocks; // This will be empty at the initial process
-} FileSystem_s; // 20 bytes
-
-typedef struct list_s {
-    // This will point to either a data section or another node
-    // This will depend on what the mode byte is
-    void *data;
-    list_s *next;
-} list_s;
+    Inode_s *current_inode; // The current inode (working directory)
+    Inode_s *previous_inode; // The previous inode (one directory level up)
+} FileSystem_s; // 28 bytes
 
 /**
-* Make an init function that will add the addresses to the free lists in the file system
+* Make an init function that will add the addresses to the free lists in the file system and set the counts
 */
-Filesystem_s* file_system_init (void);
+FileSystem_s* file_system_init (void);
 
 /**
-* Create the file system by allocating a slab for the entire thing (since we limit to 128 inodes)
-*/
-uint16_t create_file_system (FileSystem_s *fs);
+ * @brief When the user wants to create an inode, it will take a block off the free list and allocate it in the used list.
+ *        I will possibly add a function to add an indirect pointer in, but not for now.
+ * 
+ * @param fs - the file system structure
+ * @param name - the name of the inode if they want to create one
+ * @param block - the block that will go in the inode if they make a direct one
+ * @param index - the index into the array of pointers in the inode
+ * @param is_direct - if the node will be direct or indirect
+ * @param new_inode - the node if it is indirect
+ * @return Inode_s* - the node that was created for the user and added into the file system
+ */
+Inode_s *create_inode(FileSystem_s *fs, unsigned char *name, unsigned char *block, uint8_t index, bool_t is_direct, Inode_s *new_inode);
 
 /**
-* Delete the entire file system by freeing up the memory
-*/
-void delete_file_system (FileSystem_s *fs, uint32_t inode_number);
+ * @brief When the user wants to delete an object in the current inode directory, it will be added to the free list. This will
+ *        work for an indirect or direct pointer
+ * 
+ * @param fs - the file system structure
+ * @param inode - the inode the user wants to delete (if indirect)
+ * @param index - the index of the direct pointer
+ * @param is_direct - if the index is at 0, then check if it is direct or indirect
+ */
+void delete_pointer_in_inode(FileSystem_s *fs, Inode_s *inode, uint8_t index, bool_t is_direct);
 
 /**
-* Create the node by finding the first free node and add it to the used nodes list (on the end)
-*/
-void create_inode(FileSystem_s *fs, uint8_t num_pointers, bool_t is_direct);
-
-void create_data_block(FileSystem_s *fs, unsigned char block[size], uint16_t size);
-
-/**
-* Find the node in the used list and remove it
-*/
-void delete_inode(FileSystem_s *fs, Inode_s inode);
-
-/**
-* Get the inode by going into the list of inodes within the file system
-*/
-Inode_s* get_inode (FileSystem_s *fs, uint32_t inode_number);
+ * @brief Create a data block object and return it in the list object
+ * 
+ * @param fs - the file system structure
+ * @param inode - the current working directory we are in
+ * @param is_direct - if the node being added is direct or indirect
+ * @param name - the name of the file that will be created
+ * @param block - the block if it is direct
+ * @param index - the index to say where to add the node in
+ * @return list_s* 
+ */
+list_s *create_data_block(FileSystem_s *fs, Inode_s *inode, bool_t is_direct, unsigned char *name, unsigned char *block, uint8_t index);
 
 /**
-* Output what is in the file system to a data block
-*/
-void inode_read (FileSystem_s *fs, uint32_t inode_number, char *data_block, uint32_t length);
+ * @brief Read the data block from the current working directory. Check to make sure if the index is at 0, then it's direct
+ * 
+ * @param fs - the file system structure
+ * @param inode - the current working directory we are in
+ * @param inode_number - the index of the data block to read from
+ */
+void inode_read (FileSystem_s *fs, Inode_s *inode, uint32_t inode_number);
 
 /**
-* Read what is in the data block to the file
-*/
-void inode_write (FileSystem_s *fs, uint32_t inode_number, char *data_block, uint32_t length);
+ * @brief Send a data block to an inode in the current working directory
+ * 
+ * @param fs - the file system structure
+ * @param inode - the current working directory we are in
+ * @param inode_number - the index of the data block to write into
+ * @param block - the data block being sent into the inode
+ */
+void inode_write (FileSystem_s *fs, Inode_s *inode, uint32_t inode_number, unsigned char *block);
 
-Inode* move_in_directory (FileSystem_s *fs, uint32_t inode_number);
+/**
+ * @brief Set the data block of an inode to be all spaces (empty characters) when the user wants
+ * 
+ * @param fs - the file system structure
+ * @param inode - the current working directory we are in
+ * @param inode_number - the index of the data block to delete
+ */
+void inode_delete_data (FileSystem_s *fs, Inode_s *inode, uint32_t inode_number);
 
-Inode* mode_out_directory (FileSystem_s *fs, uint32_t inode_number);
+/**
+ * @brief Move into another directory by making sure the pointer at index 0 is a direct pointer
+ * 
+ * @param fs - the file system structure
+ * @param inode - the current working directory we are in
+ * @return Inode* - The current working directory we are in
+ */
+Inode_s* move_in_directory (FileSystem_s *fs, Inode_s *inode);
+
+/**
+ * @brief Print out info about the current working directory and the file system. This will include
+ *        all pointers in the current inode that are not null, and will differentiate between the first
+ *        one being a file or another directory (only first one can be). For the file system, it will print
+ *        the number of inodes in use, and not in use. It will do the same for data blocks too.
+ * 
+ * @param fs - the file system structure
+ * @param inode - the current working directory we are in
+ */
+void print_directory (FileSystem_s *fs, Inode_s *inode);
 
 #endif
